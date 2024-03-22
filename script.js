90f5eab93d2dab438afb627595924b0098d3037a
MICHAELROSENS <rrosenstrausmichael@gmail.com>
2024-03-22 14:57:10 -0400

    hh yeahhhh

---
+++ script.js
@@ -0,0 +1,39 @@
+// create an array to store names
+const namesArray = []
+
+function addName() {
+    const nameInput = document.getElementById('nameInput')
+    const name = nameInput.value.trim()
+    namesArray.push(name)
+    displayNames()
+    nameInput.value = ''
+}
+
+function displayNames() {
+    const nameList = document.getElementById('nameList')
+    nameList.innerHTML = ''
+
+    for (let i = 0; i < namesArray.length; i++) {
+        const name = namesArray[i]
+
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+
+        const span = document.createElement('span')
+        span.textContent = name
+
+        li.appendChild(span)
+
+        nameList.appendChild(li)
+    }
+
+
+
+}
+
+
+
+
+
+
+document.getElementById('addNameBtn').addEventListener('click', addName)
