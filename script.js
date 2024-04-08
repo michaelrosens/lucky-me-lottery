1beb7fdfa95d596f2acade1a9ef059e3904bbc08
MICHAELROSENS <rrosenstrausmichael@gmail.com>
2024-04-08 12:55:18 -0400

    jscript done

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// create an array to store names
-const namesArray = []
+const nameArray = [] //Create a list to store names
 
-function addName() {
-    const nameInput = document.getElementById('nameInput')
-    const name = nameInput.value.trim()
-    namesArray.push(name)
-    displayNames()
-    nameInput.value = ''
+function addName () {
+  //get a trimmed version of the name from the input box
+  const nameInput = document.getElementById('nameInput')
+  const name = nameInput.value.trim()
+  nameArray.push(name)
+  displayNames()
+  nameInput.value = ''
 }
+function displayNames(){
+    const nameList = document.getElementById('nameList')//get UL element 
+    nameList.innerHTML = '' //clears the list 
 
-function displayNames() {
-    const nameList = document.getElementById('nameList')
-    nameList.innerHTML = ''
-
-    for (let i = 0; i < namesArray.length; i++) {
-        const name = namesArray[i]
+    for (let i = 0; i < nameArray.length; i++){
+        const name = nameArray[i] //get the current name from the array
 
         const li = document.createElement('li')
         li.className = 'list-group-item'
@@ -23,11 +22,27 @@ function displayNames() {
         span.textContent = name
 
         li.appendChild(span)
-
         nameList.appendChild(li)
+
     }
 
+}
 
+
+
+function pickRandomName () {
+   
+  const randomNameDiv = document.getElementById('randomName')
+  randomNameDiv.textContent = ''
+
+  const randomNumber = Math.floor(Math.random() * nameArray.length)
+  const randomName = nameArray[randomNumber]
+
+  randomNameDiv.textContent = randomName
+
+  nameArray.splice(randomNumber, 1)
+
+  displayNames()
 
 }
 
@@ -36,4 +51,9 @@ function displayNames() {
 
 
 
+
+// add onclick to add name btn
 document.getElementById('addNameBtn').addEventListener('click', addName)
+
+// add onclick to add name btn
+document.getElementById('pickRandomBtn').addEventListener('click', pickRandomName)
